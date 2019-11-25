dc45f36f3d1026794344a13968ebdf8850fc28fa
回忆LE <1925473403@qq.com>
2019-11-25 10:14:17 +0800

    添加项目文件。

---
+++ CModule Launcher.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#define CMODULE_LAUNCHER_EXPORTS
+#include "CModule Launcher.h"
+
+int __stdcall ML_Start(const char* versions)
+{
+	MessageBoxA(0, versions,"测试",0);
+	return 0;
+}
---
+++ CModule Launcher.h
@@ -0,0 +1,10 @@
+﻿#ifndef _CMODULE_LAUNCHER_H_
+#define _CMODULE_LAUNCHER_H_
+#ifdef CMODULE_LAUNCHER_EXPORTS
+#define CMODULE_LAUNCHER extern "C" __declspec(dllexport) 
+#else
+#define CMODULE_LAUNCHER extern "C" __declspec(dllimport)
+#endif
+CMODULE_LAUNCHER int __stdcall ML_Start(const char* versions);
+#endif
+
